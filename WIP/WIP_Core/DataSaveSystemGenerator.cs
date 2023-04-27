06c16167acdbb2f375f5801c69f52ca8dd198213
NakXay KorYme <corby.maxime@gmail.com>
2023-04-27 19:49:48 +0200

    [ADD] : Generation class process done

---
+++ WIP/WIP_Core/DataSaveSystemGenerator.cs
@@ -0,0 +1,84 @@
+using KorYmeLibrary.Attributes;
+using System;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using System.Linq;
+
+namespace KorYmeLibrary.SaveSystem
+{
+    public class DataSaveSystemGenerator : MonoBehaviour
+    {
+        [SerializeField] string _dataClassName = "MyGameData";
+        string _folderName = "SaveSystemClasses";
+
+        [Button]
+        public void GenerateGameDataClass()
+        {
+            string folderPath = Application.dataPath + "/" + _folderName;
+            string systemClassName = "DSM_" + _dataClassName;
+
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+                AssetDatabase.Refresh();
+            }
+
+            if (File.Exists(folderPath + "/" + systemClassName + ".cs"))
+            {
+                Debug.LogWarning("There is already one class named this way in " + _folderName);
+                return;
+            }
+
+            // Écriture du code généré dans un fichier
+            string path = folderPath + "/" + systemClassName + ".cs";
+            string classCode =
+                "namespace KorYmeLibrary.SaveSystem " + "\n" +
+                "{" + "\n" +
+                "   public class " + systemClassName + " : DataSaveManager<" + _dataClassName + ">" + "\n" +
+                "   {" + "\n" +
+                "   }" + "\n" +
+                "\n" +
+                "   [System.Serializable]" + "\n" +
+                "   public class " + _dataClassName + " : " + "GameDataTemplate" + "\n" +
+                "   {" + "\n" +
+                "       // Create the values you want to save here" + "\n" +
+                "   }" + "\n" +
+                "}";
+
+            File.WriteAllText(path, classCode);
+            AssetDatabase.Refresh();
+        }
+
+        [Button]
+        void AttachDataSaveManager()
+        {
+            string folderPath = Application.dataPath + "/" + _folderName;
+            string systemClassName = "DSM_" + _dataClassName;
+            if (!Directory.Exists(folderPath))
+            {
+                Debug.LogWarning("No folder SaveSystemClasses has been found");
+                return;
+            }
+            if (!File.Exists(folderPath + "/" + systemClassName + ".cs"))
+            {
+                Debug.LogWarning("No game data class has been found in the folder : " + folderPath);
+                return;
+            }
+            Type type = AppDomain.CurrentDomain.GetAssemblies()
+                                .SelectMany(a => a.GetTypes())
+                                .FirstOrDefault(t => t.Name == systemClassName);
+            if (type == null)
+            {
+                Debug.LogWarning("No type has been found");
+                return;
+            }
+            if (GetComponent(type) != null)
+            {
+                Debug.LogWarning("A component already exists on this gameObject");
+                return;
+            }
+            gameObject.AddComponent(type);
+        }
+    }
+}
